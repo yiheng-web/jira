3707b6ea03c82fee09880099d1556855a4630c91
yiheng-web <1781159625@qq.com>
2025-12-01 00:31:42 +0800

    用ROW布局页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,12 +1,45 @@
 import React from 'react';
 import { ProjectListScreen } from 'screens/project-list';
 import { useAuth } from 'context/auth-context';
+import {Row} from './components/lib';
+import styled from '@emotion/styled';
 export const AuthenticatedApp = () => {
     const { logout } = useAuth();
     return (
         <div>
-            <button onClick={logout}>登出</button>
-            <ProjectListScreen />
+            <Header between={true}>
+                <HeaderLeft gap = {true}>
+                    <h2>Jira</h2>
+                    <h2>项目</h2>
+                    <h2>用户</h2>
+                </HeaderLeft>
+                <HeaderRight>
+                    <button onClick={logout}>登出</button>
+                </HeaderRight>
+            </Header>
+            <Main>
+                <ProjectListScreen />
+            </Main>
         </div>
     )
 }
+const Container = styled.div`
+display: grid;
+grid-template-rows: 6rem 1fr 6rem;
+height: 100vh;
+`
+
+const Header = styled(Row)`
+`
+const HeaderItem = styled.h3`
+margin-right: 3rem;
+`
+const HeaderLeft = styled(Row)`
+
+`
+const HeaderRight = styled.div`
+`
+const Main = styled.main`
+grid-area: main;
+height: calc(100vh - 6rem);
+`
